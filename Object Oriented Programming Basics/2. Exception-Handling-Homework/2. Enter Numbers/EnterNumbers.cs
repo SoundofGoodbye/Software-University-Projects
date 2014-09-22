e4c87a5da7ccf6bafa715ef5b1abb322789876e9
Martin Atanasov <mousesporter@abv.bg>
2014-09-22 14:22:58 +0300

    Exception handling homework. Basic use of try - catch - finally block in c#

---
+++ Object Oriented Programming Basics/2. Exception-Handling-Homework/2. Enter Numbers/EnterNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enter_Numbers
+{
+    class EnterNumbers
+    {
+        private static void Main()
+        {
+            int count = 1;
+            int x = 1;
+            int y = 5;
+
+
+            while (count <= 10)
+            {
+                Console.WriteLine("Enter number: {0}", count);
+                Console.WriteLine("Enter number between: {0} and {1}", x, y);
+                try
+                {
+                    ReadNumber(x, y);
+                    count++;
+                    x += 2;
+                    y += 5;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid input was entered - enter number again.");
+                }
+            }
+        }
+
+        private static void ReadNumber(int start, int end)
+        {
+            string userInput = Console.ReadLine();
+            int result = 0;
+
+            bool isParsed = Int32.TryParse(userInput, out result);
+
+            if (isParsed)
+            {
+                if (result < start || result > end)
+                {
+                    throw new ArgumentException("Input out of range");
+                }
+                else
+                {
+                    //Valid input.
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Ivnalid input. Not a number");
+            }
+        }
+    }
+}
