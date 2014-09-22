5531e2f187bf811df20e48d4781a13de89484c0f
Martin Atanasov <mousesporter@abv.bg>
2014-09-22 14:27:22 +0300

    First commit of this file was wrong - an empty file was commited. Commiting the working problem now.

--- Object Oriented Programming Basics/2. Exception-Handling-Homework/1. Square Root/SquareRoot.cs
+++ Object Oriented Programming Basics/2. Exception-Handling-Homework/1. Square Root/SquareRoot.cs
@@ -8,6 +8,39 @@ namespace SquareRoot
 {
     class SquareRoot
     {
-      
+        static void Main()
+        {
+            string userInput = Console.ReadLine();
+            int result = 0;
+
+
+            try
+            {
+                bool isParsed = Int32.TryParse(userInput, out result);
+                if (isParsed)
+                {
+                    if (result < 0)
+                    {
+                        throw new ArgumentException();
+                    }
+                    else
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
     }
 }
